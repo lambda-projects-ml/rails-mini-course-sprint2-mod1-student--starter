0a62a00fe36a3ab776a3e19b6bb994d171aea3ab
Mike Landers <landers.mike@gmail.com>
2019-08-20 13:56:17 -0700

    working on orders controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
-      resources :customers
-      resources :products, only: [:index, :show]
+      resources :customers do
+        resources :orders, only: [:index,:create,]
+      end
+      resources :orders, only: [:index,:show,:ship] do
+        resources :products, only: [:index, :show]
+      end
     end
   end
 end
